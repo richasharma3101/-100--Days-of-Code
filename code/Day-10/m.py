c84714e3756b0de834c1155b6e41c4740b269878
richasharma3101 <40752689+richasharma3101@users.noreply.github.com>
2018-07-18 19:57:37 +0530

    Merge Sort

---
+++ code/Day-10/m.py
@@ -0,0 +1,52 @@
+def merge(left, right, Array):
+  i = 0
+  j = 0
+  k = 0
+ 
+  while i<len(left) and j<len(right):
+    if left[i]<right[j]:
+      Array[k] = left[i]
+      i = i+1
+    else:
+      Array[k] = right[j]
+      j = j+1
+ 
+    k = k+1
+  
+  while i<len(left):
+    Array[k] = left[i]
+    i = i+1
+    k = k+1
+  
+  while j<len(right):
+    Array[k] = right[j]
+    j = j+1
+    k = k+1
+ 
+#function for dividing and calling merge function
+def mergesort(Array):
+  n = len(Array)
+  if(n<2):
+    return
+ 
+  mid = n//2
+  left = []
+  right = []
+  
+  for i in range(mid):
+    number = Array[i]
+    left.append(number)  
+   
+  for i in range(mid,n):
+    number = Array[i]
+    right.append(number)
+ 
+  mergesort(left)
+  mergesort(right)
+ 
+  merge(left,right,Array)
+ 
+#calling mergesort
+Array=[int(i) for i in input("Enter the numbers").strip().split()]
+mergesort(Array)
+print(Array)
