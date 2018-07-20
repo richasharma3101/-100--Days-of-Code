0969687769aaf32e74e783c08274ed2c5b37559a
richasharma3101 <40752689+richasharma3101@users.noreply.github.com>
2018-07-20 23:48:33 +0530

    Program to check whether a number is an Armstrong or not

---
+++ code/Day-16/armstrong.py
@@ -0,0 +1,8 @@
+n=int(input("Enter any number:"))
+a=list(map(int,str(n)))
+b=list(map(lambda x:x**3,a))
+if sum(b)==n:
+       
+       print("The number is an armstrong number")
+else:
+       print("The number is not a armstrong number")
