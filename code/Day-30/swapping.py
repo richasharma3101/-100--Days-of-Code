877b633d646d698096ffc8f7c872e9ee40a65f55
richasharma3101 <richasharma3101@github.com>
2018-08-07 23:25:43 +0530

    Swapping of two number

---
+++ code/Day-30/swapping.py
@@ -0,0 +1,7 @@
+x=input("Enter the value of x:")
+y=input("Enter the value of y:")
+temp=x
+x=y
+y=temp
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
